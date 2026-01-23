2efa0e8da1a5faa0f615689abc9ed8ca078063b4
Mark <komapc@gmail.com>
2026-01-23 15:52:43 +0200

    feat: Use Next.js standalone build for Docker and bump version to 0.1.6

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { version } = require('./package.json')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_APP_VERSION: version,
   },
